2ca9abc1c0e61dbc72aba3e909511d464ee2e81e
0bscuracaelo <147636948+0bscuracaelo@users.noreply.github.com>
2024-09-30 21:52:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".tik_tac_toe").addEventListener("click",function(){
+    window.open('/tic_tac/index.html', '_blank');
+})
+document.querySelector(".memory").addEventListener("click",function(){
+    window.open('/memory/index.html', '_blank');
+})
+document.querySelector(".typing").addEventListener("click",function(){
+    window.open('/typing/index.html', '_blank');
+})
